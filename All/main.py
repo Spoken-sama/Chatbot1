9269fba7079300e69a7a64f6fbc193739d6211c6
Spoken-sama <151039597+Spoken-sama@users.noreply.github.com>
2023-11-16 19:01:33 +0100

    V3

--- All/main.py
+++ All/main.py
@@ -1,5 +1,5 @@
-from fonction_Yrieix import *
-from fonctions_Benjamin import *
+from All.fonction_Yrieix import *
+from All.fonctions_Benjamin import *
 
 speeches = ["Nomination_Chirac1.txt", "Nomination_Chirac2.txt", "Nomination_Giscard dEstaing.txt",
             "Nomination_Hollande.txt", "Nomination_Macron.txt", "Nomination_Mitterrand1.txt",
