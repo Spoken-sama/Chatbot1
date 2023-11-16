5fe59031f610db18f0036eabb5dfca9c85afb976
yrieixx <yrieix.d.irumberry-de-salaberry@efrei.net>
2023-11-16 18:20:27 +0100

    V3

--- fonction_Yrieix.py
+++ fonction_Yrieix.py
@@ -1,7 +1,7 @@
-def extract_name_president(name) :
+def extract_name_president(name):
     president_name = name[11:]
     if ("1" in president_name) or ("2" in president_name):
-            president_name = sc
-
-
-    return
+        president_last_name = president_name[:-5]
+    else:
+        president_last_name = president_name[:-4]
+    return president_last_name
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fonction_Yrieix import *
-
+from fonctions_Benjamin import *
 
 speeches = ["Nomination_Chirac1.txt", "Nomination_Chirac2.txt", "Nomination_Giscard dEstaing.txt",
             "Nomination_Hollande.txt", "Nomination_Macron.txt", "Nomination_Mitterrand1.txt",
@@ -7,11 +7,9 @@ speeches = ["Nomination_Chirac1.txt", "Nomination_Chirac2.txt", "Nomination_Gisc
 
 last_name = []
 
-for i in speeches :
+for i in speeches:
     last_name.append(extract_name_president(i))
 
-
-from fonctions_Benjamin import *
 full_name = associating_names(last_name)
-for i in full_name :
+for i in full_name:
     print(i)
